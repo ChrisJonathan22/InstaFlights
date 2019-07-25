1ea79612d5b816b4127f9c4e6e8961dd19d24f2d
Chris Jonathan <christophereko@hotmail.fr>
2019-07-25 20:24:33 +0100

    I've installed cheerio, cors, puppeteer and unfluff. I have setup puppeteer but I haven't managed to get it to work as I need it to just yet.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 let express = require('express');
+let cors = require('cors')({ origin: true });
+let $ = require('cheerio');
+let puppeeter = require('puppeteer');
 let fetch = require('node-fetch');
+let extractor = require('unfluff');
 let flightsData;
 let port = 3000;
 let app = express();
@@ -8,13 +12,47 @@ let app = express();
 async function getFlights () {
     let fetchFlights = await fetch('https://www.skyscanner.net/transport/flights/pari/lond/190731/?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&ref=home#results'
     );
-    let json = await fetchFlights.text();
+    // let json = await fetchFlights.text();
+    let json = await extractor(fetchFlights);
     flightsData = json;
     console.log(flightsData);
     console.log(String(flightsData).indexOf('fqs-price'));
+    console.log($.load(flightsData));
 }
 
-getFlights();
+// getFlights();
+
+const scrapePrices = async username => {
+    const browser = await puppeeter.launch( { headless: true } );
+    const page = await browser.newPage();
+
+    await page.goto('https://www.skyscanner.net/transport/flights/pari/lond/190731/?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false&ref=home#results');
+
+    // await page.type('.fqs-type');
+    await page.waitFor(10000);
+
+    // await page.waitForSelector('.fqs-type', {
+    //     visible: true
+    // });
+
+    
+
+    const data = await page.evaluate( () => {
+        const priceElement = document.querySelector('span');
+        return priceElement.innerHTML;
+    });
+
+    await browser.close();
+
+    console.log(data);
+    
+    // return data;
+  
+}
+
+scrapePrices();
+
+
 
 app.get('/', (req, res) => {
     res.json({
